143aec9348e6d9673d24580978e45e7ead6ad0a8
umer2001 <umer2001.uf@gmail.com>
2020-06-24 16:41:52 +0500

    add: geo-chart

--- src/components/GeoChart.js
+++ src/components/GeoChart.js
@@ -15,10 +15,11 @@ export const GeoChart = () => {
     }, []);
 
     return (
-        <div>
+        <div className="MuiGrid-root geo-chart root test MuiGrid-item MuiGrid-grid-md-11">
+            <span className="geo-heading">Active cases World wide</span>
             <Chart
-                width={'500px'}
-                height={'300px'}
+                width={'100%'}
+                height={'auto'}
                 chartType="GeoChart"
                 rows={countryData}
                 columns={[
@@ -30,7 +31,13 @@ export const GeoChart = () => {
                       type: "number",
                       label: "active"
                     }
-                  ]}
+                ]}
+                options={{
+                    colorAxis: { maxValue: 500000, colors: ['#17a2b8'] },
+                    backgroundColor: '#282b2f',
+                    datalessRegionColor: '#6c757d',
+                    defaultColor: '#6c757d'
+                }}
                 // Note: you will need to get a mapsApiKey for your project.
                 // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
                 mapsApiKey={process.env.REACT_APP_MAPS_API_KEY}
